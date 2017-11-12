cf9acebd7da320ae87d8bfbc6c9198271f8cbe85
Paul Bailey <paul.m.bailey@gmail.com>
2017-11-12 18:38:13 +0000

    project setup

---
+++ codacation/settings/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+ENVIRONMENT = os.environ.get('ENVIRONMENT', '')
+
+if ENVIRONMENT:
+    print("Loading {} Settings".format(ENVIRONMENT.upper()))
+    
+else:
+    print("Unknown ENV Loading DEVELOPMENT Settings")
+    
+if ENVIRONMENT == 'production':
+    from codacation.settings.production import *
+    
+else:
+    from codacation.settings.development import *
+    
